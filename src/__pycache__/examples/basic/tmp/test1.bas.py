e99e83991842b84a330e67d23c81b4853a7d0551
paule32 <paule32.jk@gmail.com>
2025-01-21 15:28:40 +0100

    update

--- src/__pycache__/examples/basic/tmp/test1.bas.py
+++ src/__pycache__/examples/basic/tmp/test1.bas.py
@@ -1,37 +1,49 @@
 class main(QObject):
-    def __inir__(self):
+    def __init__(self):
         super().__init__()
         self.current_line = 10
-        self.rinning = 0
+        self.running = 0
+        self.xpos = 1
+        self.ypos = 1
+        self.console = DOSConsole()
+        self.console.win.print_line('holladiho')
         self.worker_thread = C64BasicWorkerThread()
         self.worker_thread.progress.connect(self.update_progress)
         self.worker_thread.start()
-    def update_progress(value):
-        if self.running == 2:
-            return
-        func = globals().get(f'line_{self.current_line}')
-        if func:
-            self.current_line = func()
-        else:
-            self.current_line = None
-            if self.worker_thread.isRinning():
-                self.worker_thread.terminate()
-    def line_10():
+    def update_progress(self, value):
+        while True:
+            func_name = f'line_{self.current_line}'
+            if hasattr(self, func_name):
+                self.console.win.gotoxy(self.xpos, self.ypos)
+                self.ypos += 1
+                method = getattr(self, func_name)
+                self.current_line = method()
+                if not self.current_line == None:
+                    self.current_line += 1
+            else:
+                showError(f"methode: {func_name} not found.")
+                if self.worker_thread.isRunning():
+                    self.worker_thread.terminate()
+                break
+    def line_10(self):
         next_line = None
-        console.win.print_line("HELLO WORLD")
+        self.console.win.print_line("   HELLO WORLD")
+        self.console.win.gotoxy(self.xpos, self.ypos)
+        self.ypos += 1
         if next_line is None: next_line = 40
         return next_line
-    def line_40():
+    def line_40(self):
         next_line = None
-        console.win.print_line("ENDE")
-        if next_line is None: next_line = 80
+        self.console.win.print_line("ENDE")
+        self.console.win.gotoxy(self.xpos, self.ypos)
+        self.ypos += 1
+        if next_line is None: next_line = 41
         return next_line
-    def line_80():
+    def line_41(self):
         next_line = None
-        genv.c64_parser.c64_exec_thread_stop()
-        next_line = None
+        self.console.win.print_line("OLA")
+        self.console.win.gotoxy(self.xpos, self.ypos)
+        self.ypos += 1
         return next_line
-console = DOSConsole()
-console.win.print_line('holla')
 main_func = main()
-console.exec_()
+main_func.console.exec_()
