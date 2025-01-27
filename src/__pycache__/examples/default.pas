3be41f81b28367dcad4664cde95d0df074ab07c9
paule32 <paule32.jk@gmail.com>
2025-01-27 18:30:26 +0100

    update Pascal Parser

--- src/__pycache__/examples/default.pas
+++ src/__pycache__/examples/default.pas
@@ -1,1 +1,9 @@
-// fgfg
+PROGRAM Test;
+TYPE
+  VAR x: integer;
+  VAR y, z: real;
+BEGIN
+  x := 10;
+  y := 20.5;
+  z := x + y;
+END.
--- src/__pycache__/examples/pascal/test.pas
+++
@@ -1,8 +0,0 @@
-{ this is a
-  block comment that can be appear on
-  multiple lines
-}
-// *******
-		** ddddd
-(* (* *)
-*)
